6eddf1c762637f69ea8a9909c4c1c5517d4d3809
Life is squid <rredzer000@gmail.com>
2024-02-21 23:33:59 +0000

    Blob Controller updated

--- AzureBlobTestTask/Server/Controllers/BlobController.cs
+++ AzureBlobTestTask/Server/Controllers/BlobController.cs
@@ -1,14 +1,27 @@
-﻿using AzureBlobTestTask.Server.Models;
+﻿using Application.BlobService;
+using Application.DTOs;
+using AzureBlobTestTask.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureBlobTestTask.Server.Controllers
 {
-    public class BlobController
+    public class BlobController : BaseApiController
     {
+        private readonly IBlobService _blobService;
+        private const string ContainerName = "tesktask";
+
+        public BlobController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddBlob([FromForm] BlobForm blobFormDto)
         {
-            
+            var blobDto = new BlobFormDto { Email = blobFormDto.Email, File = blobFormDto.File };
+            var blobResult = await _blobService.UploadBlobAsync(blobDto, ContainerName);
+
+            return HandleResult(blobResult);
         }
     }
 }
