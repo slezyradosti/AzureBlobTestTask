aec3c31aa623e2f5b6a5e5d0d86b65c0ccb0899b
Life is squid <rredzer000@gmail.com>
2024-02-21 23:17:13 +0000

    Sever: created skeleton of Add blob action

---
+++ AzureBlobTestTask/Server/Controllers/BlobController.cs
@@ -0,0 +1,14 @@
+﻿using AzureBlobTestTask.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobTestTask.Server.Controllers
+{
+    public class BlobController
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddBlob([FromForm] BlobForm blobFormDto)
+        {
+            
+        }
+    }
+}
