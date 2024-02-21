30238922f65f323a2fc30606ef93a28bbbcddcc8
Life is squid <rredzer000@gmail.com>
2024-02-21 23:15:55 +0000

    Application: created basic Result class Sever: create base controller for checking response result

---
+++ AzureBlobTestTask/Server/Controllers/BaseApiController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobTestTask.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        [NonAction]
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null)
+            {
+                return NotFound();
+            }
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Error);
+            }
+            if (result.IsSuccess && result.Value == null)
+            {
+                return NotFound();
+            }
+            if (result.IsSuccess && result.Value != null)
+            {
+                return Ok(result.Value);
+            }
+
+            return BadRequest();
+        }
+    }
+}
