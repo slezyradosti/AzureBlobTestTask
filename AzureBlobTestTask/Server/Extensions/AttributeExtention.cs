01e536cdee0a435e2272c2aa601b87c11d18cfeb
Life is squid <rredzer000@gmail.com>
2024-02-21 23:13:45 +0000

    Create BlobForm mode for user input Create attribute validate extention that check where users upload file with a certain extention

---
+++ AzureBlobTestTask/Server/Extensions/AttributeExtention.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureBlobTestTask.Server.Extensions
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class FileExtensionsAttribute : ValidationAttribute
+    {
+        private List<string> AllowedExtensions { get; set; }
+
+        public FileExtensionsAttribute(string fileExtensions)
+        {
+            AllowedExtensions = fileExtensions.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        /// <summary>
+        /// check file for '.fileExtension'
+        /// eg: '.png,.jpg'
+        /// </summary>
+        public override bool IsValid(object value)
+        {
+            IFormFile file = value as IFormFile;
+
+            if (file != null)
+            {
+                return AllowedExtensions.Any(x => Path.GetExtension(file.FileName) == x);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AzureBlobTestTask/Server/Models/BlobForm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureBlobTestTask.Server.Models
+{
+    public class BlobForm
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Use correct email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "File is required")]
+        [FileExtensions(".docx", ErrorMessage = "File extension must be '.docx'")]
+        public IFormFile File { get; set; }
+    }
+}
