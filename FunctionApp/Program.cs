fb3c79c20b38c25b42edb020ebc6f85f801bba86
Life is squid <rredzer000@gmail.com>
2024-02-22 00:07:42 +0000

    Azure function configuration

--- FunctionApp/Program.cs
+++ FunctionApp/Program.cs
@@ -1,4 +1,6 @@
+using FunctionApp.Data;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +10,12 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        services.AddOptions<SmtpData>()
+        .Configure<IConfiguration>((settings, configuration) =>
+        {
+            configuration.GetSection("SmtpGmailSecurity").Bind(settings);
+        });
     })
     .Build();
 
