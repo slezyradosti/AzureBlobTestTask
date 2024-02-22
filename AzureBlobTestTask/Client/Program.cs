bca8b3c7523a72c0551422fa5cbbf307f7e9eb0a
Life is squid <rredzer000@gmail.com>
2024-02-22 00:06:32 +0000

    client configuration

--- AzureBlobTestTask/Client/Program.cs
+++ AzureBlobTestTask/Client/Program.cs
@@ -1,4 +1,5 @@
 using AzureBlobTestTask.Client;
+using AzureBlobTestTask.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IBlobService, BlobService>();
 
 await builder.Build().RunAsync();
